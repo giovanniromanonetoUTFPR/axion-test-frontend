5bdfc1ef6466ad30d18bdb731f09a654a4e9fc6d
giovanniromanonetoUTFPR <giovannineto@alunos.utfpr.edu.br>
2022-06-29 21:24:52 -0300

    feat: Add route to the new pages Pessoas, Locais and Comidas

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,17 +1,28 @@
 import React from 'react';
-
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
+//Context imports
+import { AuthProvider } from '../contexts/AuthContext';
+
+//Page imports
 import { Login } from '../pages/Login';
 import { Cadastro } from '../pages/Cadastro';
+import { Comidas } from '../pages/Comidas';
+import { Locais } from '../pages/Locais';
+import { Pessoas } from '../pages/Pessoas';
 
 export function Router() {
   return (
     <BrowserRouter>
-      <Switch>
-        <Route exact path="/" component={Login} />
-        <Route exact path="/register" component={Cadastro} />
-      </Switch>
+      <AuthProvider>
+        <Switch>
+          <Route exact path="/" component={Login} />
+          <Route exact path="/register" component={Cadastro} />
+          <Route exact path="/food" component={Comidas} />
+          <Route exact path="/places" component={Locais} />
+          <Route exact path="/people" component={Pessoas} />
+        </Switch>
+      </AuthProvider>
     </BrowserRouter>
   );
 }
